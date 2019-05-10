c8dce281a1a171a956f429938281ce4eda86768f
IC Realtime MBP <ICREALTIME@ICR-MacBook-Pro.local>
2019-05-10 15:24:10 -0700

    finished afternoon

---
+++ server/getProducts.js
@@ -0,0 +1,15 @@
+const products = require('../products.json');
+
+// const getProducts = (req, res) => {
+//     res.status(200).send(products);
+// }
+
+const getProducts= (req, res) => {
+    if(req.query.price) {
+        const items = products.filter((val) => val.price >= parseInt(req.query.price));
+        return res.status(200).send(items) 
+    }
+    res.status(200).send(products)
+}
+
+module.exports = getProducts;
